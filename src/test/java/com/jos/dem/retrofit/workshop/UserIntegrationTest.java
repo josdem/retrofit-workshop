3b421eb0fbe2b1aa4dcbd4f842ec80f4a76abfe6
josdem <joseluis.delacruz@gmail.com>
2018-07-29 19:02:55 -0400

    Adding user integration test

---
+++ src/test/java/com/jos/dem/retrofit/workshop/UserIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.jos.dem.retrofit.workshop.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.jos.dem.webclient.model.PublicEmail;
+import com.jos.dem.webclient.service.UserService;
+
+import reactor.core.publisher.Flux;
+
+@ContextConfiguration(classes = WebClientApplication.class)
+@WebAppConfiguration
+public class UserIntegrationTest {
+
+  @Autowired
+  private UserService userService;
+
+  Flux<PublicEmail> getEmails() throws Exception {
+    return userService.getEmails();
+  }
+
+}
