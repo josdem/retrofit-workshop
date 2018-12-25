010df736f91205020f21812b4843126e1e5a0b7f
josdem <joseluis.delacruz@gmail.com>
2018-12-25 13:10:30 -0500

    #2 Adding label integration test

---
+++ src/test/java/com/jos/dem/retrofit/workshop/LabelIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.jos.dem.retrofit.workshop;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ContextConfiguration(classes = RetrofitWorkshopApplication.class)
+@WebAppConfiguration
+public class LabelIntegrationTest {}
