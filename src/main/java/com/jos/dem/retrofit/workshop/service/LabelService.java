ebb62826424356a5cd028c3f3b884f11c13857bd
josdem <joseluis.delacruz@gmail.com>
2018-12-28 16:00:02 -0500

    #4 Sorting imports

--- src/main/java/com/jos/dem/retrofit/workshop/service/LabelService.java
+++ src/main/java/com/jos/dem/retrofit/workshop/service/LabelService.java
@@ -1,9 +1,9 @@
 package com.jos.dem.retrofit.workshop.service;
 
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.Path;
-import retrofit2.Response;
 import retrofit2.http.POST;
 import retrofit2.http.PATCH;
 import retrofit2.http.DELETE;
--- src/main/java/com/jos/dem/retrofit/workshop/service/impl/LabelServiceImpl.java
+++ src/main/java/com/jos/dem/retrofit/workshop/service/impl/LabelServiceImpl.java
@@ -1,10 +1,10 @@
 package com.jos.dem.retrofit.workshop.service.impl;
 
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.http.Body;
 import retrofit2.http.Path;
-import retrofit2.Response;
 
 
 import javax.annotation.PostConstruct;
